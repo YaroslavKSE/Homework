39d07c7d1798a0ef46784f6032095440834f7e7d
Хомич Ярослав <94047397+YaroslavKSE@users.noreply.github.com>
2021-11-19 13:05:20 +0300

    2-improved JavaSript
    Create function sum(...args) returning sum of arguments

--- 0-buggy.js
+++ 0-buggy.js
@@ -1,4 +1,8 @@
-const firstNum = 2;
-const secondNum = 3;
-const sum = firstNum + secondNum;
-console.log('The sum is', sum);
+const sum = (...args) => {
+  let totalSum = 0;
+  for (const numbers of args) {
+      totalSum += numbers;
+    }
+  return totalSum
+};
+console.log('The sum is', sum(2, 4, 6, 20))
