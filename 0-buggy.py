b2d078a05daacd888015349be3ec4f4dcc06c227
Хомич Ярослав <94047397+YaroslavKSE@users.noreply.github.com>
2021-11-19 12:30:03 +0300

    2-improved Python
    Create function sum(*args) returning sum of arguments

--- 0-buggy.py
+++ 0-buggy.py
@@ -1,4 +1,8 @@
-first_num = 2
-secord_num = 3
-sum = first_num + second_num
-print(sum)
+def sum(*args):
+    total_sum = 0
+    for number in args:
+        total_sum += number
+    return total_sum
+
+
+print(sum(1, 5, 7, 10))
